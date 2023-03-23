7d17ca2e8f0826b4f22abc07b288e04ae3b7c288
PawleN-D <sirpawle@gmail.com>
2023-03-23 15:49:53 +0200

    db config files

---
+++ config/db.config.js
@@ -0,0 +1,19 @@
+'use strict';
+const mysql = require('mysql');
+
+const dbConn = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'node_mysql_crud_db'
+});
+
+dbConn.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Database connection established!')
+});
+
+module.exports = dbConn;
+
